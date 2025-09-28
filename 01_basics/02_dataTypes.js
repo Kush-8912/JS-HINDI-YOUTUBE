7f6296c2becf279c8dc1a05755e8a8eac8b3bea7
Kushagra Aggarwal <kushagraaggarwal76@gmail.com>
2025-09-28 10:20:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using nodejs, not browser
+console.log(3 + 3) // code readability should be high
+console.log("Kushagra")
+
+let name = "Kushagra"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// all above are primitive datatypes
+
+// object
+
+console.log(typeof "Kushagra")
+console.log(typeof 3)
+console.log(typeof isLoggedIn)
+console.log(typeof name)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+

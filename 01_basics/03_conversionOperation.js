3794f1f1e093c465e3ec960cb56233059cce1ee7
Kushagra Aggarwal <kushagraaggarwal76@gmail.com>
2025-09-28 11:29:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "33"
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// "33" => 33
+// "33abc" => NaN
+// "Kushagra" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// 21 => true
+// "" => false
+// "Kushagra" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

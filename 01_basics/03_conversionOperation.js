55df558497027cd3ca07e18f70888612beea61b1
Kushagra Aggarwal <kushagraaggarwal76@gmail.com>
2025-09-30 17:48:40 +0000

    conversions and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,62 @@
 // "" => false
 // "Kushagra" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ******************** Operations ********************
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log(2 + 2)
+// console.log(2 -2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+// let str1 = "hello "
+// let str2 = "kushagra"
+// let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2)
+// console.log(typeof("1" + 2))
+// console.log(1 + "2")
+// console.log(typeof(1 + "2"))
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(3 + 4 * 5 % 3)
+
+// console.log(true) // true
+// console.log(+ true) // 1
+// console.log(true +) // error
+// console.log(+ "") // 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+// let gameCounter1 = 100
+// ++gameCounter1
+// console.log(gameCounter1)
+
+// let gameCounter2 = 101
+// gameCounter2++
+// console.log(gameCounter2)
+
+let x = 4
+let y = x++
+console.log(x)
+console.log(y)
+
+let a = 2
+let b = ++a
+console.log(a)
+console.log(b)
+

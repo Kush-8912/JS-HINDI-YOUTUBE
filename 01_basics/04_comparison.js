e0334e5dc6636839e6827c3af3404d2aa01d7b12
Kushagra Aggarwal <kushagraaggarwal76@gmail.com>
2025-09-30 18:27:32 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0) // false
+// console.log(null == 0) // false
+// console.log(null >= 0) // true
+// In >, <, >=, <= null is converted to 0 but not in == (it is undefined there)
+
+// console.log(undefined == 0) // false
+// console.log(undefined > 0) // false
+// console.log(undefined < 0) // false
+
+// === (strict checking)
+console.log("2" === 2)

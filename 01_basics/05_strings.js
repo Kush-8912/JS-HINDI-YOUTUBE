1a8a7cfae75a84e7e772b35640602ffdf658e346
Kushagra Aggarwal <kushagraaggarwal76@gmail.com>
2025-10-01 14:41:40 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+// const name = "Kushagra"
+// const repoCount = 50
+// console.log(name + repoCount + " Value")
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`)
+
+// const gameName = new String("Counter_Strike")
+// console.log(gameName)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// let newString = gameName.substring(0,6)
+// console.log(newString)
+// let anotherString = gameName.slice(2,9)
+// console.log(anotherString)
+// let anotherString1 = gameName.slice(-28, -6)
+// console.log(anotherString1)
+
+let newStringOne = "         Kushagara             "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://kushagra.com/kushagra%20aggarwal"
+// console.log(url.replace('kushagra', 'shreyansh'))
+// console.log(url)
+console.log(url.includes("kushagra"))
+console.log(url.includes("shreyansh"))
+
+
+let strSomething = "This.is.me.Kushagra"
+console.log(strSomething.split("."))
